a8c0165663fb71e48fd15f713e1118775d9593eb
László Heim <lasolozdev@gmail.com>
2025-01-23 00:51:26 +0200

    [feat] Pixel art for the slave side

--- users/lasolozdev/rules.mk
+++ users/lasolozdev/rules.mk
@@ -1,5 +1,6 @@
 SRC += src/keyboard_pre_init_user.c \
 	src/keyboard_post_init_user.c \
+	src/oled_task_user.c \
 	src/process_record_user.c \
 	src/rgb_matrix_indicators_advanced_user.c \
 	src/features/layer_lock.c \
