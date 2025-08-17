c7e229ff234fa8ee058927b623029a4219a2595b
László Heim <lasolozdev@gmail.com>
2025-08-17 14:10:01 +0300

    [feat] Fix minor issues with the new layer & coloring

--- keyboards/splitkb/aurora/lily58/keymaps/lasolozdev/led_coloring_override.c
+++ keyboards/splitkb/aurora/lily58/keymaps/lasolozdev/led_coloring_override.c
@@ -75,15 +75,21 @@ int keycode_to_led_category(uint8_t layer, uint16_t keycode) {
         }
     } else if (layer == _ADJUST) {
         if (keycode == KC_SLEP || keycode == KC_PWR) {
+            return LED_ALT_1;
+        }
+        if (keycode >= TO(0) && keycode <= TO(15)) {
             return LED_ALT_2;
         }
-        return LED_ALT_1;
+        return LED_NORMAL;
     } else if (layer == _GAMING_1) {
         switch (keycode) {
             case KC_UP:
             case KC_LEFT:
             case KC_DOWN:
             case KC_RIGHT:
+            case KC_MPLY:
+            case KC_VOLU:
+            case KC_VOLD:
                 return LED_ALT_1;
         }
     }
